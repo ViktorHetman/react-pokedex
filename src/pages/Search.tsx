f2e16e6b8de31be358c399b69b2e0d4c23c8c0b3
Viktor <vitya.getman.79@gmail.com>
2023-05-21 13:44:40 +0300

    search for searhbar + debounce

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 
 import Wrapper from "../sections/Wrapper";
+import PokemonCard from "../components/PokemonCard";
+
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import {
 	getInitialPokemonData,
 	getRandomPokemonData,
 } from "../redux/pokemon/asyncActions";
-import PokemonCard from "../components/PokemonCard";
+import { debounce } from "../utils/debounce";
 
 const Search: React.FC = () => {
 	const dispatch = useAppDispatch();
+	const handleSearch = debounce((value: string) => getPokemons(value), 300);
 	const { allPokemons, randomPokemons } = useAppSelector(
 		({ pokemon }) => pokemon,
 	);
@@ -27,6 +30,21 @@ const Search: React.FC = () => {
 		}
 	}, [allPokemons, dispatch]);
 
+	const getPokemons = async (value: string) => {
+		if (value.length) {
+			const pokemons = allPokemons?.filter((pokemon) =>
+				pokemon.name.includes(value.toLowerCase()),
+			);
+			dispatch(getRandomPokemonData(pokemons!));
+		} else {
+			const clonedPokemons = [...(allPokemons as [])];
+			const randomPokemonsId = clonedPokemons
+				.sort(() => Math.random() - Math.random())
+				.slice(0, 20);
+			dispatch(getRandomPokemonData(randomPokemonsId));
+		}
+	};
+
 	return (
 		<>
 			<div className="search">
@@ -34,6 +52,7 @@ const Search: React.FC = () => {
 					type="text"
 					className="pokemon-search-bar"
 					placeholder="Search for pokemon"
+					onChange={(e) => handleSearch(e.target.value)}
 				/>
 				<PokemonCard pokemons={randomPokemons!} />
 			</div>
