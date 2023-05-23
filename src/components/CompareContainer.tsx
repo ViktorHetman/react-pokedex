b56c5f2a29bc013fa346e31cfda5d3d5cb5aec26
Viktor <vitya.getman.79@gmail.com>
2023-05-23 12:55:01 +0300

    displaying comparing pokemons and types and styles for buttons and for whole page

--- src/components/CompareContainer.tsx
+++ src/components/CompareContainer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FaPlus } from "react-icons/fa";
 
 import { UserPokemonType } from "../models/types";
+import { pokemonTypeInterface } from "../redux/pokemon/types";
 
 const CompareContainer = ({
 	pokemon = undefined,
@@ -10,6 +11,8 @@ const CompareContainer = ({
 	pokemon?: UserPokemonType;
 	isEmpty?: boolean;
 }) => {
+	const getStats = () => {};
+
 	return (
 		<div className="compare-container">
 			{isEmpty && (
@@ -18,6 +21,41 @@ const CompareContainer = ({
 						<FaPlus />
 					</button>
 					<h3>Add pokemon to comparison</h3>
+				</div>
+			)}
+			{pokemon && (
+				<div className="compare-element">
+					<div className="compare-info">
+						<div className="compare-details">
+							<h3>{pokemon.name}</h3>
+							<img className="compare-image" src={pokemon.image} alt="pokemon" />
+						</div>
+						<div className="pokemon-types-container">
+							<div className="pokemon-types">
+								<h4 className="pokemon-type-title">Type</h4>
+								<div className="pokemon-type-icons">
+									{pokemon?.types.map((type: pokemonTypeInterface, idx: number) => {
+										const keys = Object.keys(type);
+										return (
+											<div key={idx} className="pokemon-type">
+												<img
+													className="pokemon-type-image"
+													src={type[keys[0]].image}
+													alt="pokemon type"
+												/>
+											</div>
+										);
+									})}
+								</div>
+							</div>
+							{/* {getStats()} */}
+						</div>
+					</div>
+					<div className="compare-action-buttons">
+						<button className="compare-btn">Add</button>
+						<button className="view-btn">View</button>
+						<button className="remove-btn">Remove</button>
+					</div>
 				</div>
 			)}
 		</div>
