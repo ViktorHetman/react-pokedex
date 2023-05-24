9529bc0df61c9b0ab02a9fc302766d283a8c28cb
Viktor <vitya.getman.79@gmail.com>
2023-05-24 16:36:49 +0300

    loguot funcionality

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,13 +1,26 @@
 import React from "react";
+import { signOut } from "firebase/auth";
 import { MdOutlinePowerSettingsNew } from "react-icons/md";
 
+import { firebaseAuth } from "../utils/FirebaseConfig";
+import { useAppDispatch } from "../redux/hooks";
+import { setToasts, setUserStatus } from "../redux/app/slice";
+
 const Footer: React.FC = () => {
+	const dispatch = useAppDispatch();
+
+	const logoutHandler = () => {
+		signOut(firebaseAuth);
+		dispatch(setUserStatus(undefined));
+		dispatch(setToasts("Logged out successfully"));
+	};
+
 	return (
 		<footer>
 			<div className="block"></div>
 			<div className="data"></div>
 			<div className="block">
-				<MdOutlinePowerSettingsNew />
+				<MdOutlinePowerSettingsNew onClick={() => logoutHandler()} />
 			</div>
 		</footer>
 	);
