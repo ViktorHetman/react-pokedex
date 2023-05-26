b354824f87d4ab957de53971434b0a5be03dcbae
Viktor <vitya.getman.79@gmail.com>
2023-05-26 14:22:11 +0300

    capable moves and abilities of pokemons page

--- src/pages/PokemonPages/CapableMoves.tsx
+++ src/pages/PokemonPages/CapableMoves.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { useAppSelector } from "../../redux/hooks";
 
 const CapableMoves: React.FC = () => {
-	return <div>CapableMoves</div>;
+	const pokemonData = useAppSelector(
+		({ pokemon: { currentPokemon } }) => currentPokemon,
+	);
+	return (
+		<div className="capable-moves">
+			<h1 className="capable-moves-title">Abilities</h1>
+			<ul className="capable-moves-list ability">
+				{pokemonData?.pokemonAbilities?.abilities?.map((ability: string) => (
+					<li key={ability} className="move">
+						{ability}
+					</li>
+				))}
+			</ul>
+			<h1 className="capable-moves-title">Moves</h1>
+			<ul className="capable-moves-list">
+				{pokemonData?.pokemonAbilities?.moves?.map((move: string) => (
+					<li key={move} className="move">
+						{move}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export default CapableMoves;
